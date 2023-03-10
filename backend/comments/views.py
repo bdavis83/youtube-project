5c149d40fbe85c2fa82ade1c8e38e588ba98d133
bdavis83 <bdavis83@gmail.com>
2023-03-10 11:04:16 -0500

    added user_comments funciton to views.py in the comments app with logic

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -14,4 +14,19 @@ def get_all_comments (request):
     return Response(serializer.data)
 
 
+@api_view(["GET", "POST"])
+@permission_classes ([IsAuthenticated])
 
+def user_comments(request):
+    print ('User ', f"{request.user.id} {request.user.email} {request.user.username}")
+
+    if request.method == 'POST':
+        serializer = CommentsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response (serializer.data, status = status.HTTP_201_CREATED)
+        return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        comments = Comments.objects.filter (user_id=request.user.id)
+        serializer = CommentsSerializer (comments, many=True)
+        return Response(serializer.data)
