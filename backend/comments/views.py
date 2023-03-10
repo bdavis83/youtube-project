9b445de20f3f33dd2cb7384dfb6a629bffb3a40b
bdavis83 <bdavis83@gmail.com>
2023-03-10 10:45:15 -0500

    wrote get_all_comments function in the comments app view file

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Comments
+from serializers import CommentsSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments (request):
+    comments = Comments.objects.all()
+    serializer = CommentsSerializer(comments, many=True)
+    return Response(serializer.data)
+
+
+
