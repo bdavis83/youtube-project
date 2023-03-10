9ededf83b881bcfa91aef8edd1bd3d8df0b98262
bdavis83 <bdavis83@gmail.com>
2023-03-10 14:52:23 -0500

    fixed an issue in serializers.py in replies app

--- backend/replies/models.py
+++ backend/replies/models.py
@@ -3,6 +3,6 @@ from authentication.models import User
 
 class Reply (models.Model):
     user = models.ForeignKey (User, on_delete=models.CASCADE)
-    comment = models.ForeignKey (User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=255)
     text = models.CharField (max_length=255)
 
