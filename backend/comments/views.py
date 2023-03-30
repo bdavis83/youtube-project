9e1633737440b6fe82ff89d5ca2d291a71efbf1c
bdavis83 <bdavis83@gmail.com>
2023-03-30 10:58:55 -0400

    updated project urls + workign on comment post issue in comment views.py

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Comments
-from serializers import CommentsSerializer
+from .serializers import CommentsSerializer
+from django.shortcuts import get_list_or_404
 
 
 @api_view(['GET'])
@@ -22,10 +23,10 @@ def user_comments(request):
 
     if request.method == 'POST':
         serializer = CommentsSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save(user=request.user)
             return Response (serializer.data, status = status.HTTP_201_CREATED)
-        return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'GET':
         comments = Comments.objects.filter (user_id=request.user.id)
         serializer = CommentsSerializer (comments, many=True)
